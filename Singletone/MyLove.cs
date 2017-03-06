79f3e3b81a247b830a8154656ef06e5ee98a4cb9
DeadUser <egor.domozhirov@gmail.com>
2017-03-06 05:17:29 +0300

    Singletone completed.

---
+++ Singletone/MyLove.cs
@@ -0,0 +1,37 @@
+﻿namespace Patterns.Singleton
+{
+	using System.Data.Common;
+	using System.Drawing;
+
+	/// <summary>
+	/// класс премьер-министра.
+	/// </summary>
+	public class MyGirl
+	{
+		public Point Location { get; private set; }
+
+		private static readonly MyGirl _instance = new MyGirl(Point.Empty);
+
+		private MyGirl(Point location)
+		{
+			Location = location;
+		}
+
+		public static MyGirl Instance => _instance;
+
+		/// <summary>
+		/// Метод звонка.
+		/// </summary>
+		/// <returns></returns>
+		public string Call(string question)
+		{
+			switch (question)
+			{
+				case "": return "";
+				case "Where are u?":
+					return $"Я в {Location}";
+				default: return "";
+			}
+		}
+	}
+}
---
+++ Singletone/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singletone
+{
+	using Patterns.Singleton;
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(MyGirl.Instance.Call("Where are u?"));
+			Console.ReadLine();
+		}
+	}
+}
